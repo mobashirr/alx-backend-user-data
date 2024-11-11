700b0080f4129392761aff3c16b1133ef8b10c26
mobashirr_wsl <zfzg223@gmail.com>
2024-11-11 23:09:01 +0300

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,9 @@ This module defines the BasicAuth class, which extends the Auth class
 to implement basic authentication mechanisms for the API.
 """
 
-from api.v1.auth.auth import Auth
+from auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -37,3 +39,27 @@ class BasicAuth(Auth):
            not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Decode the Base64 part of the Authorization header into text
+        using base64 decode and utf-8 .
+
+        If the `base64_authorization_header` is not string or is none.
+        
+        Args:
+            base64_authorization_header (str): The Authorization header from the HTTP request.
+
+        Returns:
+            str: The decoded Base64 credentials portion of the header if valid.
+            None: If the header is missing, invalid, or can't be decoded".
+        """
+        if not  base64_authorization_header or \
+            not isinstance(base64_authorization_header,str):
+            return None
+        try:
+            decoded_val = base64.b64decode(base64_authorization_header).decode('utf-8')
+        except binascii.Error as e:
+            return None
+
+        return decoded_val
