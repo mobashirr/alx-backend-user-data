38989c4aad34f3f5aafbd949e22e72dbf9de5dd0
mobashirr_wsl <zfzg223@gmail.com>
2024-11-11 21:08:41 +0300

    fix syntax error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,11 +8,17 @@ from flask import request
 
 class Auth:
     
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
         return False
     
     def authorization_header(self, request=None) -> str:
         return None
     
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self, request=None):
         return None
+
+a = Auth()
+
+print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
+print(a.authorization_header())
+print(a.current_user())
