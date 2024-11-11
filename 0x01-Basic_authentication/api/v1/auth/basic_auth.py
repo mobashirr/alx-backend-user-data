a7fd2686bf7bb87086753f7c97e4b62d886ed5eb
mobashirr_wsl <zfzg223@gmail.com>
2024-11-11 22:29:24 +0300

    styling

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+"""
+Basic Authentication Module.
 
-# api/v1/auth/basic_auth.py
+This module defines the BasicAuth class, which will extend the Auth class
+to implement basic authentication mechanisms for the API.
+"""
+
 from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
+    """
+    BasicAuth class inherits from Auth and provides basic authentication.
+
+    This class currently does not implement any additional methods or attributes
+    beyond what is provided by Auth, but serves as a placeholder for
+    future functionality specific to basic authentication.
+    """
     pass
