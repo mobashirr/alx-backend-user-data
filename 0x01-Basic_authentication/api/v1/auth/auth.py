dc094ca16aa75e133c18a9af69b4c3e4f1377b1a
mobashirr_wsl <zfzg223@gmail.com>
2024-11-11 21:09:13 +0300

    del test case

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,9 +16,3 @@ class Auth:
     
     def current_user(self, request=None):
         return None
-
-a = Auth()
-
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.authorization_header())
-print(a.current_user())
